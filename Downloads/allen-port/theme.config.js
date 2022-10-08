e6cffb4611aeddc7457685a6992676d75913768f
Allen <allenliu878581@gmail.com>
2022-10-08 01:04:29 -0400

    initial commit

--- Downloads/allen-port/theme.config.js
+++ Downloads/allen-port/theme.config.js
@@ -4,8 +4,8 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Allen Liu.
+        <a href="https://cdn.guff.com/site_2/media/33000/32700/items/09a7d96cd18b3d2cae9fa2fc.jpg">bonk</a>
       </small>
       <style jsx>{`
         footer {
