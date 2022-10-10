77c508da5aecbcb95c920b4aee804803cd98fc07
Allen <allenliu878581@gmail.com>
2022-10-09 20:05:44 -0400

    new code

---
+++ theme.config.js
@@ -0,0 +1,22 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  darkMode: "false",
+  titleSuffix: " - Allen Liu",
+  footer: (
+    <footer>
+      <small>
+        <time>{YEAR}</time> © Allen Liu.
+        <a href="https://cdn.guff.com/site_2/media/33000/32700/items/09a7d96cd18b3d2cae9fa2fc.jpg">bonk</a>
+      </small>
+      <style jsx>{`
+        footer {
+          margin-top: 1rem;
+        }
+        a {
+          float: right;
+        }
+      `}</style>
+    </footer>
+  ),
+}
