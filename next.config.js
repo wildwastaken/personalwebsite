ddb25c0b6c316a2afaafeff08a498748d979c7a6
allen liu <52586172+wildwastaken@users.noreply.github.com>
2025-11-22 02:36:06 -0500

    rick roll

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ['lucide-react']
+    optimizePackageImports: ['lucide-react'],
+  },
+  redirects() {
+    return [
+      {
+        source: "/justin",
+        destination: "https://open.spotify.com/track/4PTG3Z6ehGkBFwjybzWkR8?si=629f135ebd334022",
+        permanent: true,
+      },
+    ]
   }
 }
 
